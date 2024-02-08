72f07666f702995de4f20d4ce3c6c0b8b63e1240
Alex <ab1425@canterbury.ac.uk>
2024-02-08 23:33:58 +0000

    Remove ApiDataService

--- ManagementApp/Services/JobService.cs
+++ ManagementApp/Services/JobService.cs
@@ -4,28 +4,14 @@ namespace ManagementApp.Services
 {
     public class JobService : IJobService
     {
-        private readonly ApiDataService _apiDataService;
-        public List<Job> Jobs { get; set; }
-        public JobService(ApiDataService apiDataService) 
+        public Task<List<Job>> GetAllJobs()
         {
-            _apiDataService = apiDataService;
-        }
-        public async Task<List<Job>?> GetAllJobs()
-        {
-            List<Job>? jobs = await _apiDataService.SendQuery<List<Job>>(HttpRequestMethod.GET, "/api/jobs");
-
-            // If no jobs are found, return an empty array
-            if (jobs is null)
-            {
-                return [];
-            }
-            return jobs;
+            throw new NotImplementedException();
         }
 
-        public async Task<Job?> GetJob(int jobId)
+        public Task<Job> GetJob(int jobId)
         {
-            Job? job = await _apiDataService.SendQuery<Job>(HttpRequestMethod.GET, $"/api/job/{jobId}");
-            return job;
+            throw new NotImplementedException();
         }
     }
 }
